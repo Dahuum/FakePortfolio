6253aa37b17bdeea66f01542330da1e1ad05c393
ABDURRAHMAN <123483885+Dahuum@users.noreply.github.com>
2026-02-24 10:20:30 +0100

    Comment out Navbar in App component
    Comment out Navbar component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <BrowserRouter>
     <div className="relative z-0 bg-primary">
       <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-        <Navbar />
+        {/* <Navbar /> */}
         <Hero />
 {/*         <About /> */}
       </div>
